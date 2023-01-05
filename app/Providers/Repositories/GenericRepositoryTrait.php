b8a99c3e438d3f0b5b3dd0e15a144b509d661673
GLOBAL\eregalado <edgar.regalado@g-global.com>
2023-01-04 17:11:32 -0800

    implemented GenericRepositoryTrait

---
+++ app/Providers/Repositories/GenericRepositoryTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers\Repositories;
+
+use App\Providers\Repositories\GenericUtils;
+
+trait GenericRepositoryTrait {
+    public function model() {
+        return null;
+    }
+
+    public function index() {
+        return GenericUtils::index($this->model());
+    }
+
+    public function show(int $id) {
+        return GenericUtils::show($this->model(), $id);
+    }
+
+    public function store($createBody) {
+        return GenericUtils::store($this->model(), $createBody);
+    }
+
+    public function patch(int $id, $patchBody) {
+        return GenericUtils::patch($this->model(), $id, $patchBody);
+    }
+
+    public function destroy(int $id) {
+        return GenericUtils::destroy($this->model(), $id);
+    }
+}
--- app/Providers/Repositories/ProductRepository.php
+++ app/Providers/Repositories/ProductRepository.php
@@ -6,23 +6,13 @@ use App\Models\Product;
 use App\Providers\Repositories\Interfaces\ProductRepositoryInterface;
 use App\Providers\Repositories\GenericRepository;
 
+use App\Providers\Repositories\GenericRepositoryTrait;
+
 // TODO: por el momento no incluye las imágenes, adaptar para conseguir
 // full imagenes y full reviews o quizas no, reflexionar sobre eso
-class ProductRepository extends GenericRepository implements ProductRepositoryInterface{
+class ProductRepository implements ProductRepositoryInterface {
+    use GenericRepositoryTrait;
     public function model() {
         return Product::class;
     }
 }
-/*
-class ProductRepository implements ProductRepositoryInterface
-{
-    public function show(int $id): Product {
-        //return Product::find($id);
-        return GenericUtils::show(Product::class, $id);
-    }
-
-    public function index() {
-        return GenericUtils::index(Product::class);
-    }
-}
-*/
